017b839f00c6ae21681934cdd709c8337cbcd3b8
newhorizon-tech <alaukikinbox@gmail.com>
2020-10-12 20:35:54 +0530

    Add my_count method

--- enum.rb
+++ enum.rb
@@ -42,18 +42,38 @@ module Enumerable
     end
     result
   end
+
+  def my_none?
+    result = true
+    if block_given?
+      length.times do |i|
+        result = false if yield(self[i])
+      end
+    end
+    result
+  end
+
+  def my_count
+    count = 0
+    if block_given?
+      length.times do |i|
+        count += 1 if yield(self[i])
+      end
+    end
+    count
+  end
 end
 
 arr = %w[test value test enum]
 
 puts '-' * 40
-puts "\n1. #my_each \n"
+puts "\n Enum method 1. #my_each \n"
 arr.my_each do |p|
   puts p
 end
 
 puts '-' * 40
-puts "\n2. #my_each_with_index \n"
+puts "\nEnum method 2. #my_each_with_index \n"
 arr.my_each_with_index do |p, q|
   print [p, q]
   print "\n"
@@ -63,19 +83,29 @@ puts '-' * 40
 test = [1, 2, 3, 5, 6, 8]
 
 puts '-' * 40
-puts "\n3. #my_select \n"
+puts "\n Enum method 3. #my_select \n"
 my_num = test.my_select(&:even?)
 puts my_num.inspect
 puts '-' * 40
 
-puts "\n4. #my_all \n"
+puts "\n Enum method 4. #my_all \n"
 puts %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
 puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
 puts [nil, true, 99].my_all? #=> false
 puts [].my_all? #=> true
 puts '-' * 40
 
-puts "\n5. #my_any \n"
+puts "\n Enum method 5. #my_any \n"
 puts [1, 2, 3, 5, 6, 8].my_any? { |n| n > 9 } #=> false
 puts [1, 2, 3, 5, 6, 8].my_any? { |n| n > 7 } #=> true
+puts '-' * 40
+
+puts "\n Enum method 6. #my_none \n"
+puts [1, 2, 3, 5, 6, 8].my_none? { |n| n > 9 } #=> true
+puts [1, 2, 3, 5, 6, 8].my_none? { |n| n > 7 } #=> false
+puts '-' * 40
+
+puts "\n Enum method 7. #my_count \n"
+puts [1, 2, 3, 5, 6, 8].my_count { |n| n > 4 } #=> true
+puts [1, 2, 3, 5, 6, 8].my_count { |n| n > 2 } #=> false
 puts '-' * 40
