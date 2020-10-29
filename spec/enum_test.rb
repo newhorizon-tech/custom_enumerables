188f4b32e3a6e04150cfcdd35eab5597fde56df2
Gzim <gzimasanni@gmail.com>
2020-10-29 09:32:32 +0100

    Added RSpec for my_each_with_index

--- spec/enum_test.rb
+++ spec/enum_test.rb
@@ -3,6 +3,7 @@ require_relative '../enum.rb'
 RSpec.describe Enumerable do
     let(:arr)  {[1,2,3,4]}
     let(:range){(1..10)}
+    let(:hashh){["cat", "dog", "wombat"]}
     describe "#my_each" do
         context "a block is given" do
             it "should return the element of an array" do
@@ -14,6 +15,13 @@ RSpec.describe Enumerable do
         context "a block is not given" do
             it "should return the Enumerator" do
                 expect(arr.my_each.is_a?(Enumerable)).to eql(arr.each.is_a?(Enumerable))
+            end
+        end
+    end
+    describe '#my_each_with_index' do
+        context "a block is given" do
+            it "should return the element and the index of an array" do
+                expect(hashh.my_each_with_index{|name, index| "#{name}, #{index}"}).to eql(hashh.each_with_index{|name, index| "#{name}, #{index}"})
             end
         end
     end
